95c6858762056ab8827ee339ae4e978d51aefec5
Giovanni Olivetti <88675608+OlivettiGiovanni@users.noreply.github.com>
2021-08-25 18:28:49 +0200

    fixed legend issue in plots_that_advanced

--- plots_that_advanced.py
+++ plots_that_advanced.py
@@ -9,12 +9,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.axes as ax
 
-def plots_that_advanced(x, y, y_err, fitfunc, legend_position):
+def plots_that_advanced(x, y, y_err, fitfunc):
     # legend_position can be "upper right", "upper left", "lower left", 
 	# "lower right", "right", "center left", "center right", "lower center"
 	# "upper center", "center"
-    plt.figure(dpi=1200) #better resolution
-    plt.clf()
+    #plt.figure(dpi=1200) #better resolution
+    #plt.clf()
     whichplot = input("Print lin if you want a linear scale, log if you want a log log scale. If you want a semi-log scale print linlog (xlin, ylog) o loglin(xlog, ylin)")
     plt.subplot(1, 1, 1) #specific type of plot design
     label1 = input("Write there the x axis label: ") # set the x axis label
@@ -26,8 +26,6 @@ def plots_that_advanced(x, y, y_err, fitfunc, legend_position):
     # note: the user cannot use as a legenda the string "none"
     text2 = input("Write there the fitting function legenda, if you don't want to add any legend, print none: ")
     title = input("Insert there the title of the plot, print none if you don't want any title: ")
-    if text1 != "none" and text2 != "none":
-        plt.legend(loc=legend_position) #
     grid_var = input("Press y if you want to set a grid. Otherwise, press anything else.")
     if grid_var == "y":
         plt.grid(True)
@@ -40,14 +38,16 @@ def plots_that_advanced(x, y, y_err, fitfunc, legend_position):
         plt.errorbar(x,y, yerr = y_err, fmt = 'k.', markersize = 4, label = text1, linewidth = 1)
     if whichplot == "lin":
         plt.plot(x, fitfunc, "-b", linewidth = 2, markersize = 4, label = text2)
-        plt.show()
     if whichplot == "log":
-        plt.loglog(x, fitfunc, "-b") #, linewidth = 2, markersize = 4, label = text2)
+        plt.loglog(x, fitfunc, "-b", label = text2) #, linewidth = 2, markersize = 4,)
         #ax.Axes.set_xscale('log', base = 10)
         #ax.Axes.set_xscale("log", base = 10)
-        plt.show()
     if whichplot == "linlog":
-        plt.semilogx(x, fitfunc, "-b")
+        plt.semilogx(x, fitfunc, "-b", label = text2)
     if whichplot == "loglin":
-        plt.semilogy(x, fitfunc, "-b")
+        plt.semilogy(x, fitfunc, "-b", label = text2)
+    if text1 != "none" and text2 != "none":
+        legend_position = input("Where do you want to place the legend? legend position can be upper right, upper left, lower left, lower right, right, center left, center right, lower center, upper center or center: ")
+        plt.legend(loc=legend_position)
+    plt.show()
         
