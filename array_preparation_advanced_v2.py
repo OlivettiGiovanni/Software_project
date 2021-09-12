ed37f3459ae3601c5143d33182bd4c61d262c25e
Giovanni Olivetti <88675608+OlivettiGiovanni@users.noreply.github.com>
2021-09-12 13:31:16 +0200

    zero and neg issues corrected, 3 test passed
    Simplified array_prep_v2 function and passed the three tests of test_array_prep using assert! There should be some errors before that I did not find...

--- array_preparation_advanced_v2.py
+++ array_preparation_advanced_v2.py
@@ -9,18 +9,11 @@ import numpy as np
 # Description:
 # This function automize the data extraction from a csv file basing on the 
 # headers of each column of data. 
-# The element of the output data structure <table> should be the indipendent variable
-# values x, the dipendent variable values y and the respective uncertainties y_err
-# it also transforms each uncertainty value that is zero in a value that is very small
-# compared to the average of the uncertainty. 
-# If all the uncertainties are zero... it returns an error
-
-# UN'ALTRA QUESTIONE DI CUI DOVREI TENERE CONTO è IL SEGNO DELLE INCERTEZZE,
-# HANNO SENSO INCERTEZZE NEGATIVE?? fORSE POTREI CORREGGERE IL CODICE IN MODO
-# DA CONSIDERARE SEMPRE IL MODULO.... IN VERITà, POICHè L'INCERTEZZA NEGATIVA 
-# NON HA SENSO FISICO FORSE SAREBBE MEGLIO CHE LA FUNZIONE MOSTRASSE UN ERRORE
-# INVECE CHE CORREGGERE AUTOMATICAMENTE IL DATO....
-
+# The elements of the output dataframe <result> are: the indipendent variable
+# values x, the dipendent variable values y and the respective uncertainties y_err.
+# It also transforms each uncertainty value that is zero in a value that is 
+# six order of magnitude smaller than the measured value y
+# If some uncertainties are negative, the function returns an error
 
 # Inputs:
 # the four inputs have to be strings that identify:
@@ -29,26 +22,47 @@ import numpy as np
 # dipendent variable, uncertainties on the indipendent variable
 
 # Outputs:
-# array_preparation_advanced_v2[0]: indipendent variable values
-# array_preparation_advanced_v2[1]: dipendent variable values
-# array_preparation_advanced_v2[2]: uncertainty on the dipendent variable values
+# the output is a dataframe "result" that contains the extracted data
+# result[0]: indipendent variable values
+# result[1]: dipendent variable values
+# result[2]: uncertainty on the dipendent variable values
 
 def array_preparation_advanced_v2(file_path, header1, header2, header3):
     col_list = [header1, header2, header3]
-    table = pd.read_csv (file_path, usecols = col_list)
-    y_err = table[header3]
-    meanerr = np.mean(y_err)
-    n = len(y_err)
-    numb = list(range(n))
-    for i in numb:
-        if y_err[i] == 0 and meanerr != 0:
-            y_err[i] = meanerr / 100
+    table = pd.read_csv(file_path, usecols = col_list)
     x = table[header1]
     y = table[header2]
-    #lets rebuild a dataframe in order to return it as an output
+    y_err = table[header3]
+    #let's prepare the variable for the cycles
+    n = len(x)
+    numbers = list(range(n))
+    #check if some uncertainty is negative
+    how_many_neg = 0
+    for i in numbers:
+        if y_err[i] < 0:
+            how_many_neg = how_many_neg+1
+    #checl if some uncertainty is zero
+    how_many_zero = 0
+    for i in numbers:
+        if y_err[i] == 0:
+            how_many_zero = how_many_zero+1
+            y_err[i] = y[i] / 1000000
     x_series = pd.Series(x)
     y_series = pd.Series(y)
     y_err_series = pd.Series(y_err)
     frame = {"col1": x_series, "col2":y_series, "col3":y_err_series}
     result = pd.DataFrame(frame)
-    return result
+    if how_many_zero == 0 and how_many_neg == 0:
+        return result
+    if how_many_neg > 0:
+        raise ValueError("Some uncertainties are negative, therefore not acceptable. Check your data!")
+    if how_many_zero > 0 and how_many_neg == 0:
+        print("Some uncertainties are equal to zero and have been replaced with negligible values. Check your data! The number of uncertainties equal to zero is" + how_many_zero)
+        return result
+
+
+
+
+
+
+
--- test_array_prep2.py
+++ test_array_prep2.py
@@ -7,49 +7,75 @@ Created on Sat Sep 11 12:57:34 2021
 
 from array_preparation_advanced_v2 import array_preparation_advanced_v2
 import pandas as pd
-#import numpy as np
-#import hypothesis
 from hypothesis import given
 import hypothesis.strategies as st
+import pytest
 
 data_number = 10
 @given(list1 = st.lists(elements=st.floats(width = 16, min_value = -10, max_value = 10, allow_infinity=False, allow_nan = False), min_size = 15, max_size = 15), list2 = st.lists(elements=st.floats(width = 16, min_value = -10, max_value = 10, allow_infinity=False, allow_nan = False), min_size = 15, max_size = 15), list3 = st.lists(elements=st.floats(width = 16, min_value = 0, max_value = 10, allow_infinity=False, allow_nan = False, exclude_min = True), min_size = 15, max_size = 15))
 # le variabili selezionate da hypothesis.strategy sono sottoposte ai seguenti
-# constrains: limitato il loro valore, crea problemi con numeri estremamente bassi
-# (5e-323), con +-inf e con nan, dunque ho limitato i valori del dominio senza
-# perdita di generalità. I vettori di input dovrebbero infatti essere valori finiti
-# e con ordini di grandezza che non toccano il limite inferiore per i float
-# manipolabili di python. 
-# Ho ridotto il numero di bit usati per generare i float a 16 per evitare 
-# che gli arrotondamenti di excel o qualunque programma di editing influenzino
-# le relazioni di uguaglianza usate per validare la funzione
+# constrains: not allowed +-inf e nan, non significativi per delle incertezze
+#I vettori di input dovrebbero infatti essere valori finiti
 # La dimensione degli array di input è stata fissata in modo da risultare 
-# uguale per tutti e tre i vettori, lasciandola non vincolata il pc talvolta
-# si bloccava (forse a causa di array troppo lunghi)
+# uguale per tutti e tre i vettori
 def test_array_prep_output_par2(list1, list2, list3):
-    # data_numb is the number of experimental point we have in our dataset
-    # let's generate random numbers for our experimental data
-    #list_1 = np.zeros(data_numb) + 2*np.random.randn(data_numb)
-    #list_2 = np.zeros(data_numb) + 1.5*np.random.randn(data_numb)
-    #list_3 = np.zeros(data_numb) + np.random.randn(data_numb)
+    #a problem can be the number of significant digits, when you save data into csv file
+    #the number of significant digits may be reduced. Let's impose it is equal to 6
+    #in order to be sure any error rises due to this difference
+    n = len(list1)
+    numbers = list(range(n))
+    for i in numbers:
+        list1[i] = float("{0:.6g}".format(list1[i]))
+        list2[i] = float("{0:.6g}".format(list2[i]))
+        list3[i] = float("{0:.6g}".format(list3[i]))
+    # then we can build a dataframe of data in order ot save them in a csv file
     data_th = pd.DataFrame(data={"col1": list1, "col2": list2, "col3": list3})
     # let's create a csv file that will be used as an input of  the 
     # array_preparation function
-    data_th.to_csv("./dati3.csv", sep=',',index=False)
+    data_th.to_csv("./dati_par2.csv", sep=',',index=False)
     #let's collect the output of the array_preparation function
-    data = array_preparation_advanced_v2("dati3.csv", "col1", "col2", "col3")
-    #let's generate a dataframe from the comparison between the 
-    #array_preparation output and the set input
+    data = array_preparation_advanced_v2("dati_par2.csv", "col1", "col2", "col3")
+    #let's generate a dataframe of boolean variable from the comparison between the 
+    #array_preparation output and the set input.
     ausilio = (data_th == data)
     # from the dataframe let's obtain a tuple whose element are all 
-    # boolean variable 
+    # boolean variable, the tuple will contain all True value if each column
+    # of data_th is equal to the respective column of data
     for index, row in ausilio.iterrows():
         ausilio1 =(row["col1"], row["col2"], row["col3"])
     # if all the elements of ausilio1 are equal we obtain a one element set
-    # whose element is the shared one!
+    # whose element is the shared one.
     ausilio2 = set(ausilio1)
     # if this element is the boolean variable True, the test is passed
-    bol = (ausilio2 == {True})
-    assert bol#, "data not correctly read"
-    #if ausilio2 == {True}:
-    #    pass
+    assert ausilio2 == {True}
+
+    
+    
+@given(list1 = st.lists(elements=st.floats(width = 16, min_value = 0, max_value = 10, exclude_min = True, allow_infinity=False, allow_nan = False), min_size = 15, max_size = 15), list2 = st.lists(elements=st.floats(width = 16, min_value = 0, max_value = 10, exclude_min = True, allow_infinity=False, allow_nan = False), min_size = 15, max_size = 15), list3 = st.lists(elements=st.floats(width = 16, min_value = 0, max_value = 0, allow_infinity=False, allow_nan = False), min_size = 15, max_size = 15))  
+# in this case hypothesis keep the same constrain of the previous test function 
+# for both list1 and list2. For the uncertainties list I fixed their value to zero
+def test_prep_zero(list1, list2, list3):
+    data_th = pd.DataFrame(data={"col1": list1, "col2": list2, "col3": list3})
+    data_th.to_csv("./dati_zero.csv", sep=',',index=False)
+    data = array_preparation_advanced_v2("dati_zero.csv", "col1", "col2", "col3")
+    y_err = data["col3"]
+    n = len(y_err)
+    numbers = list(range(n))
+    ausilio = 0
+    for i in numbers:
+        if y_err[i] == 0:
+            ausilio = ++ausilio
+    assert ausilio == 0
+    
+@given(list1 = st.lists(elements=st.floats(width = 16, min_value = 0, max_value = 10, exclude_min = True, allow_infinity=False, allow_nan = False), min_size = 15, max_size = 15), list2 = st.lists(elements=st.floats(width = 16, min_value = 0, max_value = 10, exclude_min = True, allow_infinity=False, allow_nan = False), min_size = 15, max_size = 15), list3 = st.lists(elements=st.floats(width = 16, min_value = -1, max_value = 0, exclude_max = True, allow_infinity=False, allow_nan = False), min_size = 15, max_size = 15))
+# in this case the uncertainties list contains only negative values
+def test_rep_neg(list1, list2, list3):  
+    data_th = pd.DataFrame(data={"col1": list1, "col2": list2, "col3": list3})
+    data_th.to_csv("./dati_zero.csv", sep=',',index=False)
+    with pytest.raises(ValueError):
+        array_preparation_advanced_v2("dati_zero.csv", "col1", "col2", "col3")
+    
+# MISSED: RAISE ERROR WHEN THE NUMBER OF ELEMENT OF THE THREE INPUTS IS NOT CORRECT!!
+            
+    
+    
