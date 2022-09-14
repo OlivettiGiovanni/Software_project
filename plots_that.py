3363ca495df54d5d3b72f070a84425d4da618ea4
Giovanni Olivetti <88675608+OlivettiGiovanni@users.noreply.github.com>
2022-09-14 18:56:46 +0200

    preparing plots_that in order to use config_file

--- plots_that.py
+++ plots_that.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 23 16:37:53 2021
-
-@author: Giovanni Olivetti
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Tue Aug 24 18:22:17 2021
 
 @author: Giovanni Olivetti
 """
+
 
 import matplotlib.pyplot as plt
 
@@ -30,29 +24,17 @@ import matplotlib.pyplot as plt
 #Output:
 # personalized plot (all the possible choises are clearly explained)
 
-def plots_that(x, y, y_err, fitfunc, plot):
-    # It is possible to chose a preselected plot or personalize each of its aspects
-    # If plot == True, user instruction are required (command line)
-    if plot == True:
-        whichplot = input("Print lin if you want a linear scale, log if you want a log log scale. If you want a semi-log scale print linlog (xlin, ylog) o loglin(xlog, ylin)")
-        label1 = input("Write there the x axis label: ") # set the x axis label
-        lebel2 = input("Write there the y axis label: ") # set the y axis label
+def pltos_that(x, y, y_err, fitfunc):
+    if plot = True:
         plt.xlabel(label1)
-        plt.ylabel(lebel2)
-        # insert the legenda (it's not mandatory)
-        text1 = input("Write there the experimental values legenda, if you don't want to add any legend, print none: ")
-        # note: the user cannot use as a legenda the string "none"
-        text2 = input("Write there the fitting function legenda, if you don't want to add any legend, print none: ")
-        title = input("Insert there the title of the plot, print none if you don't want any title: ") #title
-        grid_var = input("Press y if you want to set a grid. Otherwise, press anything else.") #grid off/on
+        plt.ylabel(lebel2)        
         if grid_var == "y":
             plt.grid(True)
         if title != "none":
-            plt.title(title) # title
-        capvar = input("Do you want that the uncertainty bars have a cap? [y/n]") # error bars uncertainties
-        if capvar == "y":
+            plt.title(title)
+        if capunc == "y":
             plt.errorbar(x,y, yerr = y_err, fmt = 'k.', markersize = 4, label = text1, capsize = 3, linewidth = 1)
-        if capvar == "n":
+        if capunc == "n":
             plt.errorbar(x,y, yerr = y_err, fmt = 'k.', markersize = 4, label = text1, linewidth = 1)
         if whichplot == "lin":
             plt.plot(x, fitfunc, "-b", linewidth = 2, markersize = 4, label = text2)
@@ -63,13 +45,11 @@ def plots_that(x, y, y_err, fitfunc, plot):
         if whichplot == "loglin":
             plt.semilogy(x, fitfunc, "-b", label = text2)
         if text1 != "none" and text2 != "none":
-            legend_position = input("Where do you want to place the legend? legend position can be upper right, upper left, lower left, lower right, right, center left, center right, lower center, upper center or center: ")
             plt.legend(loc=legend_position)
         plt.show()
-    # If plot == False, a predefined simple plot is sketched (useful for fast preliminary analysis)
-    if plot == False:
-        plt.xlabel("x")
-        plt.ylabel("y")
-        plt.errorbar(x,y, yerr = y_err, fmt = 'k.', markersize = 4, capsize = 3, linewidth = 1)
-        plt.plot(x, fitfunc, "-b", linewidth = 2, markersize = 4)
-        plt.show()
+     if plot == False:
+         plt.xlabel("x")
+         plt.ylabel("y")
+         plt.errorbar(x,y, yerr = y_err, fmt = 'k.', markersize = 4, capsize = 3, linewidth = 1)
+         plt.plot(x, fitfunc, "-b", linewidth = 2, markersize = 4)
+         plt.show()   
